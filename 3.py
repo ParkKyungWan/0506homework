109c3c9b22cf500fb546c81100b3b4a6bd770fe6
ParkKyungWan <pkw8117197@gmail.com>
2020-05-07 00:35:56 +0900

    fix

--- 3.py
+++ 3.py
@@ -1,12 +1,12 @@
 N = int(input());
-answer =0;
+answer = 0;
 i = 2
-while i<N//2+1:
-    ㄱㅇㄱ =1
-    while N%i ==0:
-        ㄱㅇㄱ *= i
-        N//=i
-    if ㄱㅇㄱ>answer:
-        answer = ㄱㅇㄱ
-    i+=1
-print(answer if answer>N else N)
+while i<N // 2+1:
+    j = 1
+    while N % i == 0:
+        j *= i
+        N //= i
+    if j > answer:
+        answer = j
+    i += 1
+print(answer if answer > N else N)
--- 4.py
+++ 4.py
@@ -1,8 +1,8 @@
 answer =0
 for i in range(999,0,-1):
     for j in range(999,0,-1):
-        ㅋ = i*j
-        ㄱ = str(ㅋ)
-        if ㄱ == ㄱ[::-1] and ㅋ>answer:
-            answer = ㅋ
+        곱한값 = i*j
+        문자_곱한값 = str(곱한값)
+        if 문자_곱한값 == 문자_곱한값[::-1] and 곱한값>answer:
+            answer = 곱한값
 print(answer)
